e10a9332dbbe856e2aa9099d9553630423e094f3
Marcin Krasowski <marcin.krasowski@hycom.pl>
2025-03-06 12:43:52 +0100

    fix(docs): fixed incorrect docusaurus config

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -461,25 +461,27 @@ const config: Config = {
     } satisfies Preset.ThemeConfig,
 
     themes: [
-        hideDocs
+        ...(hideDocs
             ? []
             : [
-                  require.resolve('@easyops-cn/docusaurus-search-local'),
-                  /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-                  {
-                      // ... Your options.
-                      // `hashed` is recommended as long-term-cache of index file is possible.
-                      hashed: true,
+                  [
+                      require.resolve('@easyops-cn/docusaurus-search-local'),
+                      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+                      {
+                          // ... Your options.
+                          // `hashed` is recommended as long-term-cache of index file is possible.
+                          hashed: true,
 
-                      // For Docs using Chinese, it is recomended to set:
-                      // language: ["en", "zh"],
+                          // For Docs using Chinese, it is recomended to set:
+                          // language: ["en", "zh"],
 
-                      // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
-                      // forceIgnoreNoIndex: true,
+                          // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+                          // forceIgnoreNoIndex: true,
 
-                      indexBlog: false,
-                  },
-              ],
+                          indexBlog: false,
+                      },
+                  ],
+              ]),
     ],
 };
 
