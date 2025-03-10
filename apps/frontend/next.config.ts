758d45056487ea40716b768988e0764189a3241d
Jan Jagoda <jan.jagoda@hycom.pl>
2025-03-10 11:32:55 +0100

    fix: content related fixes

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -22,6 +22,10 @@ const nextConfig: NextConfig = {
                 protocol: 'https',
                 hostname: 'avatars.githubusercontent.com',
             },
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+            },
         ],
     },
     sassOptions: {
