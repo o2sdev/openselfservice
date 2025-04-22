f010aa57a63e690b6fc8a123c6825c9310090d8b
tomaszpacior <tomasz.paciorkowski@hycom.pl>
2025-04-22 19:28:27 +0200

    fix: surveyJS - small fixes

--- apps/frontend/src/blocks/SurveyJs/SurveyJs.renderer.tsx
+++ apps/frontend/src/blocks/SurveyJs/SurveyJs.renderer.tsx
@@ -1,7 +1,6 @@
 import { useLocale } from 'next-intl';
 import React, { Suspense } from 'react';
 
-import { Container } from '@/components/Container/Container';
 import { Loading } from '@/components/Loading/Loading';
 
 import { SurveyJs } from './SurveyJs.server';
@@ -15,17 +14,7 @@ export const SurveyJsRenderer: React.FC<SurveyJsRendererProps> = ({ id, accessTo
     const locale = useLocale();
 
     return (
-        <Suspense
-            key={id}
-            fallback={
-                <>
-                    <Loading bars={1} />
-                    <Container variant="narrow">
-                        <Loading bars={8} />
-                    </Container>
-                </>
-            }
-        >
+        <Suspense key={id} fallback={<Loading bars={8} />}>
             <SurveyJs id={id} accessToken={accessToken} locale={locale} />
         </Suspense>
     );
