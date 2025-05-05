bb4653655020d33558de81f702be6fa15ed709a0
Tomasz Paciorkowski <tomasz.paciorkowski@hycom.pl>
2025-05-05 11:22:24 +0200

    feat: cases submission (#95)
    * feat: cases submission
    
    * feat: add scrollable toggle group filter with multiple selection
    
    -  Add scroll container for toggle group filter in overlay view
    - Implement multiple selection version for toggle group filter
    - Add support for horizontal scrolling in filter items
    - Improve filter item layout with proper spacing and alignment
    - Add new components: ScrollContainer and ToggleGroup
    - Add shx script for better cross-platform shell compatibility
    - Add proper styling for filter items in scroll container
    
    ---------
    Co-authored-by: Aneta Sliwinska <aneta.sliwinska@hycom.pl>
    
    * fix: changing links in recent tickets
    
    Co-authored-by: Aneta Sliwinska <aneta.sliwinska@hycom.pl>
    
    * fix: coherence of action buttons in tables and change styles in rich text (#97)
    
    Co-authored-by: Aneta Sliwinska <aneta.sliwinska@hycom.pl>
    
    * fix: code review fixes
    
    * chore: add scripts for update dependencies (#94)
    
    * chore: add scripts for update dependencies
    
    * chore: removed individual scripts from each package
    
    * chore: fixed internal dependencies versions
    
    * chore: updated dependencies
    
    * chore: updated dependencies
    
    ---------
    
    Co-authored-by: Marcin Krasowski <marcin.krasowski@hycom.pl>
    
    * feat: cases submission
    
    * fix: code review fixes
    
    * chore: dependency fix
    
    ---------
    
    Co-authored-by: Aneta <51403632+anetasliwinska@users.noreply.github.com>
    Co-authored-by: Aneta Sliwinska <aneta.sliwinska@hycom.pl>
    Co-authored-by: Marcin Krasowski <marcin.krasowski@hycom.pl>

--- packages/integrations/mocked/src/modules/cms/mappers/cms.survey.mapper.ts
+++ packages/integrations/mocked/src/modules/cms/mappers/cms.survey.mapper.ts
@@ -18,7 +18,16 @@ const MOCK_SURVEY_2: CMS.Model.Survey.Survey = {
     postId: 'e0f1b26b-a434-44ab-9608-c49dcd0658ec',
 };
 
-const MOCK_SURVEYS = [MOCK_SURVEY_1, MOCK_SURVEY_2];
+const MOCK_SURVEY_3: CMS.Model.Survey.Survey = {
+    code: 'request-device-maintenance',
+    surveyId: 'd93ccc83-4aff-418b-9e9b-c9c3447908cf',
+    surveyType: 'survey',
+    submitDestination: ['surveyjs'],
+    requiredRoles: ['selfservice_user'],
+    postId: '17931fe3-2492-408c-8f91-8fc062606604',
+};
+
+const MOCK_SURVEYS = [MOCK_SURVEY_1, MOCK_SURVEY_2, MOCK_SURVEY_3];
 
 export const mapSurvey = (code: string): CMS.Model.Survey.Survey => {
     return MOCK_SURVEYS.find((survey) => survey.code === code) ?? MOCK_SURVEY_1;
