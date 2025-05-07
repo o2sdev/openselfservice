8afeb0d1a5a635080235a967e25f7a8c48e1f09e
Aneta Sliwinska <aneta.sliwinska@hycom.pl>
2025-05-07 12:45:21 +0200

    feat: article page - removing title from page template

--- apps/frontend/src/blocks/Article/Article.client.tsx
+++ apps/frontend/src/blocks/Article/Article.client.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Separator } from '@o2s/ui/components/separator';
 import { Typography } from '@o2s/ui/components/typography';
 
 import { Author } from '@/components/Author/Author';
@@ -13,45 +14,46 @@ export const ArticlePure: React.FC<Readonly<ArticlePureProps>> = ({ ...component
     const {
         data: { author, createdAt, sections },
     } = component;
-
+    console.log(component);
     return (
-        <div className="w-full flex flex-col gap-4">
-            {component.__typename}: {component.id}
-            <a href="https://markdown-it.github.io/">https://markdown-it.github.io/</a>
-            <div className="border p-4">
+        <div className="w-full flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+                <Typography variant="h1" asChild>
+                    <h1>{component.data.title}</h1>
+                </Typography>
                 {author && <Author name={author.name} avatar={author.avatar} position={author.position} />}
-
-                {createdAt}
-
-                <Container variant="narrow">
-                    <ul className="flex flex-col gap-4">
-                        {sections.map((section) => {
-                            switch (section.__typename) {
-                                case 'ArticleSectionText':
-                                    return (
-                                        <li key={section.id} className="border p-4">
-                                            <Typography variant="h2">{section.__typename}</Typography>
-                                            <RichText content={section.content} />
-                                        </li>
-                                    );
-                                case 'ArticleSectionImage':
-                                    return (
-                                        <li key={section.id} className="border p-4">
-                                            <Typography variant="h2">{section.__typename}</Typography>
-                                            <Image
-                                                src={section.image.url}
-                                                alt={section.image.alt}
-                                                width={section.image.width}
-                                                height={section.image.height}
-                                            />
-                                            <Typography>{section.caption}</Typography>
-                                        </li>
-                                    );
-                            }
-                        })}
-                    </ul>
-                </Container>
+                <Typography variant="p">{createdAt}</Typography>
             </div>
+            <Separator />
+
+            <Container variant="narrow">
+                <ul className="flex flex-col gap-8">
+                    {sections.map((section) => {
+                        switch (section.__typename) {
+                            case 'ArticleSectionText':
+                                return (
+                                    <li key={section.id} className="border p-4">
+                                        <Typography variant="h2">{section.__typename}</Typography>
+                                        <RichText content={section.content} />
+                                    </li>
+                                );
+                            case 'ArticleSectionImage':
+                                return (
+                                    <li key={section.id} className="border p-4">
+                                        <Typography variant="h2">{section.__typename}</Typography>
+                                        <Image
+                                            src={section.image.url}
+                                            alt={section.image.alt}
+                                            width={section.image.width}
+                                            height={section.image.height}
+                                        />
+                                        <Typography>{section.caption}</Typography>
+                                    </li>
+                                );
+                        }
+                    })}
+                </ul>
+            </Container>
         </div>
     );
 };
