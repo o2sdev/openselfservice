4973c08731a5cee348efd1867ede4469bd332ac7
tomaszpacior <tomasz.paciorkowski@hycom.pl>
2025-05-26 13:33:12 +0200

    fix: code-review fixes

--- apps/api-harmonization/src/blocks/article-search/article-search.controller.ts
+++ apps/api-harmonization/src/blocks/article-search/article-search.controller.ts
@@ -3,6 +3,8 @@ import { Controller, Get, Headers, Query, UseInterceptors } from '@nestjs/common
 import { LoggerService } from '@o2s/utils.logger';
 import { Observable } from 'rxjs';
 
+import { Auth } from '@o2s/framework/modules';
+
 import { AppHeaders } from '@o2s/api-harmonization/utils/headers';
 
 import { ArticleList } from './article-search.model';
@@ -15,11 +17,13 @@ export class ArticleSearchController {
     constructor(protected readonly service: ArticleSearchService) {}
 
     @Get()
+    @Auth.Decorators.Roles({ roles: [] })
     getArticleSearchBlock(@Headers() headers: AppHeaders, @Query() query: GetArticleSearchBlockQuery) {
         return this.service.getArticleSearchBlock(query, headers);
     }
 
     @Get('articles')
+    @Auth.Decorators.Roles({ roles: [] })
     searchArticles(@Headers() headers: AppHeaders, @Query() query: SearchArticlesQuery): Observable<ArticleList> {
         return this.service.searchArticles(query, headers);
     }
