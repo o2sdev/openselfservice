5ede3e530a412c2fe580a8d9303c462f87d85a36
Jan Jagoda <jan.jagoda@hycom.pl>
2025-06-17 15:32:03 +0200

    docs: website homepage changes

--- apps/docs/src/components/GetStartedButtons/index.tsx
+++ apps/docs/src/components/GetStartedButtons/index.tsx
@@ -1,21 +1,30 @@
 import clsx from 'clsx';
 import React, { useState } from 'react';
 
+import GithubActiveIcon from '@site/src/assets/icons/github-active.svg';
+import GithubIcon from '@site/src/assets/icons/github.svg';
 import { Body } from '@site/src/components/Typography';
 
 const GetStartedButtons = () => {
     return (
         <div>
-            <div className="flex gap-6 max-w-md">
-                <a href="/docs" className={clsx('w-full md:w-auto button')}>
-                    Get started
+            <div className="sm:flex gap-6">
+                <a className={clsx('button button mb-4')} href="https://demo.openselfservice.com" target="_blank">
+                    See our demo app
                 </a>
                 <a
-                    href="https://demo.openselfservice.com"
+                    href="https://github.com/o2sdev/openselfservice"
+                    className={clsx('button button-ultra')}
                     target="_blank"
-                    className={clsx('w-full md:w-auto button button-secondary')}
+                    rel="noopener"
                 >
-                    See our demo app
+                    <span className="label flex items-center justify-center gap-2">
+                        <span className="relative h-5 w-5 mr-2 flex-shrink-0">
+                            <GithubIcon className="absolute inset-0 h-5 w-5 transition-opacity duration-200 pointer-events-none github-icon-default" />
+                            <GithubActiveIcon className="absolute inset-0 h-5 w-5 transition-opacity duration-200 pointer-events-none github-icon-active" />
+                        </span>
+                        Star us on GitHub
+                    </span>
                 </a>
             </div>
         </div>
