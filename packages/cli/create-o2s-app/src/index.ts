1694c5d8a99da2361ed7137978c10a3e9ac572e1
Marcin Krasowski <marcin.krasowski@hycom.pl>
2025-09-01 12:22:53 +0200

    fix: updated script to remove unneeded files

--- packages/cli/create-o2s-app/src/index.ts
+++ packages/cli/create-o2s-app/src/index.ts
@@ -75,8 +75,10 @@ program
             rmSync(`${targetDirectory}/packages/framework`, { recursive: true });
             rmSync(`${targetDirectory}/packages/integrations`, { recursive: true });
             rmSync(`${targetDirectory}/packages/blocks`, { recursive: true });
+            rmSync(`${targetDirectory}/packages/modules`, { recursive: true });
             rmSync(`${targetDirectory}/packages/utils`, { recursive: true });
             rmSync(`${targetDirectory}/packages/cli`, { recursive: true });
+            rmSync(`${targetDirectory}/packages/telemetry`, { recursive: true });
 
             let file = readFileSync(targetDirectory + '/package-lock.json', 'utf8');
             file = removeSymLinks(file);
