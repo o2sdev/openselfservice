9c62515019b564f0f761ec7c4da1d7ea51e54b10
Marcin Krasowski <marcin.krasowski@hycom.pl>
2025-09-02 12:53:43 +0200

    fix(integrations.mocked): removed Polish characters from mocked URLs for servivces

---
+++ .changeset/ready-showers-say.md
@@ -0,0 +1,5 @@
+---
+'@o2s/integrations.mocked': patch
+---
+
+removed Polish characters from mocked URLs for servivces
--- packages/integrations/mocked/src/modules/cms/mappers/blocks/cms.featured-service-list.mapper.ts
+++ packages/integrations/mocked/src/modules/cms/mappers/blocks/cms.featured-service-list.mapper.ts
@@ -59,7 +59,7 @@ const MOCK_FEATURED_SERVICE_LIST_BLOCK_PL: CMS.Model.FeaturedServiceListBlock.Fe
         title: 'Nie znaleziono usług',
         description: 'Nie znaleziono usług spełniających Twoje kryteria',
     },
-    detailsUrl: '/usługi/{id}',
+    lsUrl: '/uslugi/{id}',
     labels: {
         on: 'Włącz',
         off: 'Wyłącz',
