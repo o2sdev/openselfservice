182e9aba032c29898987428b699d6fb276d98105
lukasz.bielecki <lukasz.bielecki@hycom.pl>
2025-11-04 16:31:37 +0100

    docs(pages): adding product page with basic structure

---
+++ apps/docs/src/pages/product/integrations.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Layout from '@theme/Layout';
+
+import styles from './product.module.scss';
+
+export default function ProductIntegrations() {
+    return <Layout title="Integrations">integrations page here</Layout>;
+}
---
+++ apps/docs/src/pages/product/starters.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Layout from '@theme/Layout';
+
+import styles from './product.module.scss';
+
+export default function ProductStarters() {
+    return <Layout title="Starters">starters page here</Layout>;
+}
