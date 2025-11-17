05021f0a5986f40578b7b519f82f7c750415102f
lukasz.bielecki <lukasz.bielecki@hycom.pl>
2025-11-17 11:58:26 +0100

    feat(docs): add Accordion component into FeaturesListWithImage

--- apps/docs/src/components/FeaturesListWithImage/index.tsx
+++ apps/docs/src/components/FeaturesListWithImage/index.tsx
@@ -3,6 +3,7 @@ import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState } from 'react';
 
 import ChevronRightIcon from '@site/src/assets/icons/ChevronRight.svg';
+import Accordion from '@site/src/components/Accordion';
 import { H3 } from '@site/src/components/Typography';
 
 interface FeaturesListWithImageProps {
@@ -76,12 +77,14 @@ export function FeaturesListWithImage({ title, features, link }: FeaturesListWit
             {/* Mobile/Tablet layout: Single column */}
             <div className="flex flex-col gap-8 lg:hidden w-full">
                 <H3 className="mb-0!">{title}</H3>
-                {features.map((feature, featureIndex) => (
-                    <div key={featureIndex} className="flex flex-col gap-4">
-                        <h4 className="m-0! font-bold! text-base! leading-6! text-white">{feature.title}</h4>
-                        <img src={feature.image} alt={feature.title} className="w-full h-auto rounded-lg" />
-                    </div>
-                ))}
+                <Accordion
+                    items={features.map((feature, featureIndex) => ({
+                        title: feature.title,
+                        content: <img src={feature.image} alt={feature.title} className="w-full h-auto rounded-lg" />,
+                        value: `feature-${featureIndex}`,
+                    }))}
+                    type="single"
+                />
             </div>
         </div>
     );
