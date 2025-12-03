03541261cf1ea3b194eea47b4983354f300f9279
Marcin Krasowski <marcin.krasowski@hycom.pl>
2025-12-03 16:01:57 +0100

    feat: dev task optimization to reduce CPU/RAM load (#419)
    * feat: optimized dev tasks to limit nodejs processes
    
    * chore: updated Next.js to v16
    
    * chore: deduped package tree
    
    * fix: reverted env variable where which used `https`
    
    * feat: optimized dev tasks to limit nodejs processes
    
    * feat: optimized dev tasks to limit nodejs processes
    
    * feat: optimized dev tasks to limit nodejs processes
    
    * chore: formatting fix
    
    * feat: optimized dev tasks to limit nodejs processes
    
    * fix: upgraded to the new way of linting nextjs app
    
    * fix: updated lit tasks to include both ts and tsx files
    
    * fix: added missing package
    
    * chore: added changeset

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -14,6 +14,7 @@ const nextConfig: NextConfig = {
     output: 'standalone',
     images: {
         // deviceSizes: [430, 828, 1200, 2048, 3840],
+        qualities: [75, 90],
         remotePatterns: [
             {
                 protocol: 'https',
@@ -37,6 +38,7 @@ const nextConfig: NextConfig = {
         silenceDeprecations: ['legacy-js-api'],
     },
     experimental: {
+        turbopackFileSystemCacheForDev: true,
         // dynamicIO: true,
         // cacheLife: {
         //     render: {
