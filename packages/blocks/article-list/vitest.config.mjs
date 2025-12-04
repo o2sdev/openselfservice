3b2b3bb4a16ee9d70f855695396950b548fd403d
Michal Nowak <michal.nowak@hycom.pl>
2025-12-04 14:48:07 +0100

    feat: add @o2s/vitest-config package and update vitest configuration for article-list

--- packages/blocks/article-list/vitest.config.mjs
+++ packages/blocks/article-list/vitest.config.mjs
@@ -1,28 +1,3 @@
-import { resolve } from "node:path";
-import swc from "unplugin-swc";
-import { defineConfig } from "vitest/config";
+import { config } from "@o2s/vitest-config/block";
 
-export default defineConfig({
-    test: {
-        name: "api",
-        globals: true,
-        root: "./",
-        include: ["src/api-harmonization/**/*.spec.ts"],
-        environment: "node",
-        exclude: [
-            "**/node_modules/**",
-            "**/dist/**",
-            "**/.{idea,git,cache,output,temp}/**",
-        ],
-    },
-    plugins: [
-        swc.vite({
-            module: { type: "es6" },
-        }),
-    ],
-    resolve: {
-        alias: {
-            src: resolve(__dirname, "./src"),
-        },
-    },
-});
+export default config;
