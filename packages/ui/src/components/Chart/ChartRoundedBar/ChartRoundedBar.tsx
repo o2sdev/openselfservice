bc07cab0169008c23c77e6242bba63797b7f8cdc
tomaszpacior <tomasz.paciorkowski@hycom.pl>
2025-12-18 14:15:53 +0100

    fix(deps): update dependency recharts to v3

--- packages/ui/src/components/Chart/ChartRoundedBar/ChartRoundedBar.tsx
+++ packages/ui/src/components/Chart/ChartRoundedBar/ChartRoundedBar.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react';
-import { Props } from 'recharts/types/cartesian/Bar';
+import { Bar } from 'recharts';
 
 import { ChartRoundedBarProps } from './ChartRoundedBar.types';
 
-export const ChartRoundedBar: FC<Props & ChartRoundedBarProps> = (props) => {
+type BarProps = React.ComponentProps<typeof Bar>;
+
+export const ChartRoundedBar: FC<BarProps & ChartRoundedBarProps> = (props) => {
     const {
         x: xString,
         y: yString,
