b8d0f0ec23bf02a351297def170c901040a35395
Michał Nowak <49277278+michnowak@users.noreply.github.com>
2025-12-19 15:10:43 +0100

    feat:storybook vite migration (#410)
    * feat: migrate Storybook to use nextjs-vite and update dependencies
    
    * feat: migrate Storybook to use nextjs-vite and update dependencies
    
    * feat: integrate Vitest for testing Storybook components and update dependencies
    
    * feat: add Storybook test for article list and update notification details status
    
    * feat: enhance Storybook configuration
    
    * fix: reorder Storybook test script in package.json
    
    * fix: update Storybook configuration to use process.env as an object
    
    * fix: update Storybook build scripts for improved performance
    
    * feat: integrate SWC and Vitest for api-harmonization and sdk testing
    
    * feat: update test scripts and add PageService tests for api-harmonization
    
    * feat: add @o2s/vitest-config package and update vitest configuration for article-list
    
    * fix: correct test script syntax in package.json for proper execution
    
    * chore: remove article-list unit tests as they are no longer needed
    
    * chore: update test configurations
    
    * chore: remove unnecessary newline in vitest configuration for user account
    
    * feat: add vitest configuration and unit tests for article and ticket-details mappers
    
    * feat: add vitest test scripts and configuration to multiple package.json files
    
    * feat: add unit tests for various mappers and update vitest configuration
    
    * fix: update package-lock.json nad downgrade playwright module.
    
    * fix: add posinstall command
    
    * fix: updated build and tst scripts in some blocks
    
    * fix: added missing config files
    
    * fix: fixed client files
    
    * fix: remove postinstall script
    
    * chore: update package-lock.json and package.json for dependency management
    
    ---------
    
    Co-authored-by: Marcin Krasowski <marcin.krasowski@hycom.pl>
    Co-authored-by: tomaszpacior <tomasz.paciorkowski@hycom.pl>

--- packages/ui/src/elements/table.stories.tsx
+++ packages/ui/src/elements/table.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/nextjs';
+import type { Meta, StoryObj } from '@storybook/nextjs-vite';
 
 import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from './table';
 
