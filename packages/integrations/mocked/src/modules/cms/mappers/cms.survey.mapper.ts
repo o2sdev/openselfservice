4092ffd40558647eae7fed650e6253225cf0b91f
lukasz.bielecki <lukasz.bielecki@hycom.pl>
2026-01-28 19:05:21 +0100

    fix(survey.mapper): update submitDestination from 'tickets' to 'surveyjs' for mock surveys

--- packages/integrations/mocked/src/modules/cms/mappers/cms.survey.mapper.ts
+++ packages/integrations/mocked/src/modules/cms/mappers/cms.survey.mapper.ts
@@ -4,7 +4,7 @@ const MOCK_SURVEY_1: CMS.Model.Survey.Survey = {
     code: 'contact-us',
     surveyId: '72c90a02-6bfe-4e83-ba48-01f11752c234',
     surveyType: 'survey',
-    submitDestination: ['tickets'],
+    submitDestination: ['surveyjs'],
     requiredRoles: [],
     postId: 'a91349b1-0c4c-4b7a-b712-91f04a1e6e99',
 };
@@ -13,7 +13,7 @@ const MOCK_SURVEY_2: CMS.Model.Survey.Survey = {
     code: 'complaint-form',
     surveyId: '3897de9c-279b-4c50-b359-09f5c73a3c49',
     surveyType: 'survey',
-    submitDestination: ['tickets'],
+    submitDestination: ['surveyjs'],
     requiredRoles: ['selfservice_org_user'],
     postId: 'e0f1b26b-a434-44ab-9608-c49dcd0658ec',
 };
@@ -22,7 +22,7 @@ const MOCK_SURVEY_3: CMS.Model.Survey.Survey = {
     code: 'request-device-maintenance',
     surveyId: 'd93ccc83-4aff-418b-9e9b-c9c3447908cf',
     surveyType: 'survey',
-    submitDestination: ['tickets'],
+    submitDestination: ['surveyjs'],
     requiredRoles: ['selfservice_org_user'],
     postId: '17931fe3-2492-408c-8f91-8fc062606604',
 };
