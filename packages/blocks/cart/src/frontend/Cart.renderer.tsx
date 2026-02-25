77f27cce699b850854fa1f4c595260948b31f438
lukasz.bielecki <lukasz.bielecki@hycom.pl>
2026-02-25 16:49:41 +0100

    refactor(cart): simplify CartRenderer by removing loading components

--- packages/blocks/cart/src/frontend/Cart.renderer.tsx
+++ packages/blocks/cart/src/frontend/Cart.renderer.tsx
@@ -1,9 +1,6 @@
 import { useLocale } from 'next-intl';
 import React, { Suspense } from 'react';
 
-import { Container } from '@o2s/ui/components/Container';
-import { Loading } from '@o2s/ui/components/Loading';
-
 import { Cart } from './Cart.server';
 import { CartRendererProps } from './Cart.types';
 
@@ -11,20 +8,8 @@ export const CartRenderer: React.FC<CartRendererProps> = ({ id, accessToken, rou
     const locale = useLocale();
 
     return (
-        <Suspense
-            key={id}
-            fallback={
-                <>
-                    <Loading bars={1} />
-                    <Container variant="narrow">
-                        <Loading bars={8} />
-                    </Container>
-                </>
-            }
-        >
+        <Suspense key={id}>
             <Cart id={id} accessToken={accessToken} locale={locale} routing={routing} hasPriority={hasPriority} />
         </Suspense>
     );
 };
-
-export { CartRenderer as default };
