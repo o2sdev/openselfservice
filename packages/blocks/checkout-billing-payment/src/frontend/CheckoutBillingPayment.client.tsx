65512362d17d15d3c40b038b4060a39803b84a85
lukasz.bielecki <lukasz.bielecki@hycom.pl>
2026-03-10 11:14:51 +0100

    refactor(checkout): simplify payment provider retrieval by using optional regionId

--- packages/blocks/checkout-billing-payment/src/frontend/CheckoutBillingPayment.client.tsx
+++ packages/blocks/checkout-billing-payment/src/frontend/CheckoutBillingPayment.client.tsx
@@ -85,14 +85,12 @@ export const CheckoutBillingPaymentPure: React.FC<Readonly<CheckoutBillingPaymen
                     });
                 }
                 setCartPromotions(cart.promotions);
-                if (cart.regionId) {
-                    const providers = await sdk.payments.getProviders(
-                        { regionId: cart.regionId },
-                        { 'x-locale': locale },
-                        accessToken,
-                    );
-                    setPaymentProviders(providers.data ?? []);
-                }
+                const providers = await sdk.payments.getProviders(
+                    { ...(cart.regionId && { regionId: cart.regionId }) },
+                    { 'x-locale': locale },
+                    accessToken,
+                );
+                setPaymentProviders(providers.data ?? []);
                 if (cart.paymentMethod) {
                     setInitialFormValues({ paymentMethod: cart.paymentMethod.id });
                 }
